08d602335ed5d2ea6ed5971c3eba085d0bcf31e4
snow6098 <77281784+zam6098@users.noreply.github.com>
2021-03-13 23:34:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+cd ~
+sudo cp -r /home/$USER/discordautosetup/ /opt/
+sudo chmod +x discord.sh 
+sudo chmod +x alertsqlid.sh
+sudo ln -s /opt/discordautosetup/discord.sh /usr/local/bin/todiscord
+sudo ln -s /opt/discordautosetup/alertsqlid.sh /usr/local/bin/alert
+
+
 echo "Enter Discord Bot ID"
 read botid
 echo "Enter Webhook Personal URL"
