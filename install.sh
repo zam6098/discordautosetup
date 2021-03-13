de7a510c1720c51670d805aaffdfd154de3571f0
snow6098 <77281784+zam6098@users.noreply.github.com>
2021-03-14 00:03:36 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 cd ~
 sudo cp -r /home/$USER/discordautosetup/ /opt/
 sudo chmod +x /opt/discordautosetup/discord.sh 
-sudo chmod +x /opt/discordautosetup/alertsqlid.sh
+sudo chmod +x /opt/discordautosetup/alertpublic.sh
+sudo chmod +x /opt/discordautosetup/alertpersonal.sh
 sudo ln -s /opt/discordautosetup/discord.sh /usr/local/bin/todiscord
-sudo ln -s /opt/discordautosetup/alertsqlid.sh /usr/local/bin/alert
+sudo ln -s /opt/discordautosetup/alertpersonal.sh /usr/local/bin/alert
+sudo ln -s /opt/discordautosetup/alertpublic.sh /usr/local/bin/alert2
 
 
 echo "Enter Discord Bot ID"
