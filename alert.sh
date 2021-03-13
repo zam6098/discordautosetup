b409335e4bdea0855a096684322ad9c41f3ecada
snow6098 <77281784+zam6098@users.noreply.github.com>
2021-03-14 00:10:53 +0800

    Create alert.sh

---
+++ alert.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+tmux new -d -s alert
+tmux new-window -t alert:1 -n alert1
+tmux new-window -t alert:2 -n alert2
+tmux send -t alert:alert1 'alertpersonal' ENTER
+tmux send -t alert:alert2 'alertpublic' ENTER
