134cc11b3a9644b2ab804b09a8c327e2399153b0
snow6098 <77281784+zam6098@users.noreply.github.com>
2021-03-15 19:08:50 +0800

    Update alertpersonal.sh

--- alertpersonal.sh
+++ alertpersonal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-tail -n 10 -f ~/.local/share/sqlmap/output/results* | stdbuf -o0 grep -vE 'Target|unexploitable' | stdbuf -o0 sed -e 's|^[^/]//||' -e 's/==>//' -e '/^\s*$/d' -e 's/<==//' -e 's/:\b[0-9]\{2,3\}\b//' | stdbuf -o0 awk -F ',' '{print$1}' | stdbuf -o0 grep '^h' | xargs -I@ sh -c "todiscord --webhook-url='$WEBHHOOKPERSONAL'  --username '$IDSCANNER' --title '@' --footer '$IDSCANNER' --timestamp --avatar 'https://i.imgur.com/JooGWlF.jpg' --author 'Found New SQL Injection' "
+tail -f -n1 ~/.local/share/sqlmap/output/results* | stdbuf -o0 grep -vE 'Target|unexploitable' | stdbuf -o0 sed -e 's|^[^/]//||' -e 's/==>//' -e '/^\s*$/d' -e 's/<==//' -e 's/:\b[0-9]\{2,3\}\b//' | stdbuf -o0 awk -F ',' '{print$1}' | stdbuf -o0 grep '^h' | xargs -I@ sh -c "todiscord --webhook-url='$WEBHHOOKPERSONAL'  --username '$IDSCANNER' --title '@' --footer '$IDSCANNER' --timestamp --avatar 'https://i.imgur.com/JooGWlF.jpg' --author 'Found New SQL Injection' "
 
 
