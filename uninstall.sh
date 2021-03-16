12eb5bc219cfe292c27d26c1fa67e4c451a2805d
snow6098 <77281784+zam6098@users.noreply.github.com>
2021-03-16 13:08:31 +0800

    Create uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd ~
+sudo rm -rf /opt/discordautosetup/ 
+
+sudo rm -rf /usr/local/bin/todiscord
+sudo lrm -rf /usr/local/bin/result
+sudo rm -rf /usr/local/bin/alertpersonal
+sudo rm -rf /usr/local/bin/alertpublic
+sudo rm -rf /usr/local/bin/alert
+
+
+
