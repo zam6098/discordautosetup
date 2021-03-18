eccb877c7a8ef3cde5d1551ba0981d5cd1b3e4d7
snow6098 <77281784+zam6098@users.noreply.github.com>
2021-03-18 15:54:07 +0800

    Update result.sh

--- result.sh
+++ result.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-cat ~/.local/share/sqlmap/output/results* | stdbuf -o0 grep -vE 'Target|unexploitable' | stdbuf -o0 sed -e 's|^[^/]//||' -e 's/==>//' -e '/^\s*$/d' -e 's/<==//' -e 's/:\b[0-9]\{2,3\}\b//' | stdbuf -o0 awk -F ',' '{print$1}' | stdbuf -o0 grep '^h' 
-
+cat $(ls -t ~/.local/share/sqlmap/output/results* ) |  stdbuf -o0 grep -vE 'Target|unexploitable' | stdbuf -o0 sed -e 's|^[^/]//||' -e 's/==>//' -e '/^\s*$/d' -e 's/<==//' -e 's/:\b[0-9]\{2,3\}\b//' | stdbuf -o0 awk -F ',' '{print$1}' | stdbuf -o0 grep '^h'
 
